e6e412da410bbc2863b5599271e28186ac37d5a5
Rob3rt2 <robert.mota@etec.sp.gov.br>
2023-10-25 12:08:18 -0300

    criando base da forca

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
-const canvas = document.getElementById(canvas);
+class jogo {
+
+        constructor(tipo){
+
+            this.tipo = tipo;
+        }
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
 
+ctx.beginPath();
+ctx.moveTo(100, 500);
+ctx.lineTo(150, 400);
+ctx.lineTo(200,500);
+ctx.lineTo(100, 500);
+ctx.moveTo(150,400);
+ctx.lineTo(150,150);
+ctx.lineTo(300, 150);
+ctx.strokeStyle = "black";
+ctx.stroke();
