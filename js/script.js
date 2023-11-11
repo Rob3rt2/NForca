908bb3ff35fa8ab68d6cc56803b0dbca0e07667e
Rob3rt2 <robert.mota@etec.sp.gov.br>
2023-11-10 21:45:47 -0300

    corrigindo problemas de letras e adicionando o boneco

--- js/script.js
+++ js/script.js
@@ -1,88 +1,198 @@
 class Forca {
     constructor() {
-        this.animais = ['cobra', 'arara', 'burro', 'macaco', 'abelha', 'capivara'];
-        this.palavraS = this.animais[Math.floor(Math.random() * this.animais.length)];
-        this.letraDescoberta = "";
+        this.animais = ['arara', 'cobra', 'burro', 'macaco', 'abelha', 'capivara'];
+        this.palavraS = this.animais[Math.floor(Math.random() * this.animais.length)].toUpperCase();
+        this.letraDescoberta = new Set();
         this.letraErrada = 0;
         this.letraCor = 0;
+        this.maxErros = 6;
     }
 
     gerarPalavra() {
-        for (var i = 0; i < this.palavraS.length; i++) {
-            ctx.moveTo(290 + 150 * i, 490);
-            ctx.lineTo(300 + 150 * i, 500);
-            ctx.lineTo(400 + 150 * i, 500);
-            ctx.lineTo(410 + 150 * i, 490);
-        }
+        const canvas = document.getElementById("canvas");
+        const ctx = canvas.getContext("2d");
+        ctx.canvas.width = window.innerWidth;
+        ctx.canvas.height = window.innerHeight;
+
+        // Desenhar o suporte da forca
+        ctx.beginPath();
+        ctx.font = "40px Arial";
+        
+        ctx.fillText("Letras Usadas: ", 800, 600);
+
+        ctx.beginPath();
+        ctx.font = "40px Verdana";
+        ctx.fillText("  ANIMAL ", 50, 600);
+
+        ctx.beginPath();
+        ctx.font = "40px Verdana";
+        ctx.fillText(" FORCA ", 50, 100)
+
+        ctx.beginPath();
+        ctx.moveTo(110, 500);
+        ctx.lineTo(100, 490);
+        ctx.lineTo(150, 400);
+        ctx.lineTo(200, 490);
+        ctx.lineTo(190, 500);
+        ctx.lineTo(110, 500);
+        ctx.moveTo(150, 400);
+        ctx.lineTo(150, 170);
+        ctx.lineTo(170, 150);
+        ctx.lineTo(280, 150);
+        ctx.lineTo(300, 170);
+        ctx.lineTo(300, 200);
+        ctx.strokeStyle = "black";
         ctx.stroke();
+        ctx.stroke();
+        ctx.stroke();
+
+        this.desenharPalavra();
     }
 
     verificarPalpite(event) {
-        if (this.letraDescoberta.includes(event.key) == this.palavraS.includes(event.key.toUpperCase())) {
-            this.desenharLetra(event.key.toUpperCase(), 290 + 150 * i, 200);
-            for (var i = 0; i < this.palavraS.length; i++) {
-                if (this.palavraS[i] == event.key.toUpperCase()) {
-                    ctx.font = "50px Verdana";
-                    ctx.fillText(event.key.toUpperCase(), 290 + 150 * i, 200);
-                    this.letraCor++;
-                }
+        const letra = event.key.toUpperCase();
+
+        if (this.palavraS.includes(letra)) {
+            if (!this.letraDescoberta.has(letra)) {
+                this.letraCor++;
+                this.letraDescoberta.add(letra);
+                this.desenharLetra(letra);
             }
         } else {
-            this.desenharLetra();
             this.letraErrada++;
-            // this.boneco(letrasErradas);  // Comente ou remova essa linha se não houver um método chamado boneco
+           
+        }
+        this.letras
+        this.verificarLetra();
+    }
+
+    desenharLetra(letra) {
+        const canvas = document.getElementById("canvas");
+        const ctx = canvas.getContext("2d");
+        ctx.font = "50px Verdana";
+        for (let i = 0; i < this.palavraS.length; i++) {
+            if (this.palavraS[i] === letra) {
+                ctx.fillText(letra, 400 + 100 * i, 480);
+            }
         }
     }
 
-    desenharLetra() {
-        this.letraDescoberta = this.letraDescoberta + event.key;
-        ctx.font = "30px Verdana";
-        ctx.fillText(this.letraDescoberta.toUpperCase(), 700, 650);
+    desenharPalavra() {
+        const canvas = document.getElementById("canvas");
+        const ctx = canvas.getContext("2d");
+        ctx.font = "50px Verdana";
+        for (let i = 0; i < this.palavraS.length; i++) {
+            ctx.fillText("_", 400 + 100 * i, 480);
+        }
+    }
+
+    verificarLetra() {
+        const canvas = document.getElementById("canvas");
+        const ctx = canvas.getContext("2d");
+        if (this.letraErrada >= this.maxErros) {
+            ctx.font = "30px Verdana";
+            ctx.fillText(`0 vidas, fim de jogo. A palavra correta era ${this.palavraS}`, 700, 100);
+            document.removeEventListener('keydown', this.verificarPalpite);
+        } else if (this.letraCor == this.palavraS.length) {
+            ctx.font = "30px Verdana";
+            ctx.fillText("Palavra completa! Parabéns!!!", 700, 100);
+            document.removeEventListener('keydown', this.verificarPalpite);
+        }
     }
 }
 
-// Inicializando o canvas
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-ctx.canvas.width = window.innerWidth;
-ctx.canvas.height = window.innerHeight;
+class boneco{
+    constructor(){
+            this.posicaoX = 300;
+            this.posicaoY= 220;
+            
+    }
 
-// Desenhando o boneco e outros elementos do jogo
-function desenharBoneco() {
-    ctx.beginPath();
-    // Adicione aqui o código para desenhar o boneco
-    ctx.stroke();
+    desenharCabeca(ctx){
+            ctx.beginPath();
+            ctx.arc(this.posicaoX, this.posicaoY, 20, 0, Math.PI * 2, true);
+            ctx.stroke();
+            ctx.stroke();
+            ctx.stroke();
+        
+
+    }
+    desenharTronco(ctx){
+        ctx.beginPath();
+            ctx.moveTo(this.posicaoX ,this.posicaoY + 20);
+            ctx.lineTo(this.posicaoX , this.posicaoY + 120);
+            ctx.stroke();
+            ctx.stroke();
+            ctx.stroke();
+        
+    }
+    desenharBracoD(ctx){
+        ctx.beginPath();
+       ctx.moveTo(this.posicaoX, this.posicaoY + 30);
+       ctx.lineTo(this.posicaoX + 40, this.posicaoY + 70);
+       ctx.stroke();
+       ctx.stroke();
+       ctx.stroke();
+    
+    }
+    desenharBracoE(ctx){
+        ctx.beginPath();
+            ctx.moveTo(this.posicaoX, this.posicaoY + 30);
+            ctx.lineTo(this.posicaoX - 40, this.posicaoY + 70);
+            ctx.stroke();
+            ctx.stroke();
+            ctx.stroke();
+        
+    }
+    desenharPernaD(ctx){
+        ctx.beginPath();
+            ctx.moveTo(this.posicaoX, this.posicaoY + 120);
+            ctx.lineTo(this.posicaoX + 50, this.posicaoY + 200 );
+            ctx.stroke();
+            ctx.stroke();
+            ctx.stroke();
+        
+    }
+    desenharPernaE(ctx){
+        ctx.beginPath();
+            ctx.moveTo(this.posicaoX, this.posicaoY +120);
+            ctx.lineTo(this.posicaoX - 50, this.posicaoY + 200);
+            ctx.stroke();
+            ctx.stroke();
+            ctx.stroke();
+        
+    }
 }
 
-ctx.beginPath();
-ctx.font = "40px Arial ";
-ctx.fillText("letras erradas", 800, 600);
+const forcaboneco = new boneco();
+const jogoForca = new Forca();
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.moveTo(110, 500);
-ctx.lineTo(100, 490);
-ctx.lineTo(150, 400);
-ctx.lineTo(200, 490);
-ctx.lineTo(190, 500);
-ctx.lineTo(110, 500);
-ctx.moveTo(150, 400);
-ctx.lineTo(150, 170);
-ctx.lineTo(170, 150);
-ctx.lineTo(280, 150);
-ctx.lineTo(300, 170);
-ctx.lineTo(300, 200);
-ctx.strokeStyle = "black";
-ctx.stroke();
-ctx.stroke();
-ctx.stroke();
-ctx.stroke();
 
-// Criando uma única instância de Forca
-const forca = new Forca();
 
-// Adicionando um ouvinte de eventos de teclado
+jogoForca.gerarPalavra();
+
 document.addEventListener('keydown', function(event) {
-    forca.verificarPalpite(event);
+    jogoForca.verificarPalpite(event);
+    switch (jogoForca.letraErrada) {
+        case 1:
+            forcaboneco.desenharCabeca(ctx);
+            break;
+        case 2:
+            forcaboneco.desenharTronco(ctx);
+            break;
+        case 3:
+            forcaboneco.desenharBracoD(ctx);
+            break;
+        case 4:
+            forcaboneco.desenharBracoE(ctx);
+            break;
+        case 5:
+            forcaboneco.desenharPernaD(ctx);
+            break;
+        case 6:
+            forcaboneco.desenharPernaE(ctx);
+            break;
+    }
 });
-
-forca.gerarPalavra();
